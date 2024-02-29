f046610e9df60c44e8a3423f887f4c4e30e3d0c6
Cabeludo <brepimentel2001@gmail.com>
2024-02-29 16:35:45 -0300

    alteração da msg com funcionalidade de botão de beijos

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,30 @@
+import { useState } from "react"
 export default () => {
-    return <h1>Minha namorada é incrivel!!!</h1>
+  const [beijoCount, setBeijo] = useState(0)
+
+  const renderBeijos = () => {
+    let beijos = [];
+    for (let i = 1; i < beijoCount; i++) {
+      const element = "😘";
+      beijos.push(element);
+    }
+    return beijos;
+  }
+  return (
+    <>
+      <h1> Para Gabi </h1>
+      <h3> Sei que você ta cansada meu amor </h3>
+      <h3> Sei que a faculdade ta dificil </h3>
+      <h3> Sei que você ta se esforçando muito no trabalho </h3>
+      <h3> Sei que a gente não tem ficado tão grudadinhos </h3>
+      <h3> Mas já já a gente vai conseguir nossas coisinhas e vamos ficar juntinhos 🥰 </h3>
+      <h4> Mas se ainda estiver tristinha </h4>
+      <h4> Clica no botão pra receber muitos beijos 🥰</h4>
+      <button onClick={() => setBeijo(beijoCount + 1)}>😍😘</button>
+      <br />
+      
+      {renderBeijos()}
+    </>
+  )
 }
+
