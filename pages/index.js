e4f7667851404d77b137a883542490dcea39b9b2
Cabeludo <brepimentel2001@gmail.com>
2024-02-29 16:59:24 -0300

    frase site

--- pages/index.js
+++ pages/index.js
@@ -14,12 +14,12 @@ export default () => {
     <>
       <h1> Para Gabi </h1>
       <h3> Sei que você ta cansada meu amor </h3>
-      <h3> Sei que a faculdade ta dificil </h3>
+      <h3> Sei que a faculdade ta difícil</h3>
       <h3> Sei que você ta se esforçando muito no trabalho </h3>
       <h3> Sei que a gente não tem ficado tão grudadinhos </h3>
       <h3> Mas já já a gente vai conseguir nossas coisinhas e vamos ficar juntinhos 🥰 </h3>
       <h4> Mas se ainda estiver tristinha </h4>
-      <h4> Clica no botão pra receber muitos beijos 🥰</h4>
+      <h4> Clica no botão para receber muitos beijos 🥰</h4>
       <button onClick={() => setBeijo(beijoCount + 1)}>😍😘</button>
       <br />
       
