f2b602f8a6a8624a54be05121891672224e7484b
Cabeludo <brepimentel2001@gmail.com>
2024-03-07 01:22:58 -0300

    add variaveis .env e modulo `database.js`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,3 +1,7 @@
-export default function status(req, res) {
+import database from "../../../../infra/database.js";
+
+export default async function status(req, res) {
+  const result = await database.query('SELECT 1 as tas;')
+  console.log(result.rows);
   res.status(200).json({ chave: "Sou acima da media" });
 }
