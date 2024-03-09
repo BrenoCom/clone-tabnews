b0793b60bad2649551977fdcf864a9b484fab66d
Cabeludo <brepimentel2001@gmail.com>
2024-03-09 00:53:39 -0300

    finaliza endpoint de status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,26 @@
 import database from "infra/database.js";
 
 export default async function status(req, res) {
-  const result = await database.query('SELECT 1 as tas;')
-  console.log(result.rows);
-  res.status(200).json({ chave: "Sou acima da media" });
+
+  const dbVersion = await database.query("SHOW server_version;");
+
+  const dbMaxCons = await database.query("SHOW MAX_CONNECTIONS;");
+
+  const databaseName = process.env.POSTGRES_DB;
+  const dbCurrentCons = await database.query({
+    text:"SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName]
+  });
+
+  const updatedAt = new Date().toISOString();
+  res.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: dbVersion.rows[0].server_version,
+        max_connections: parseInt(dbMaxCons.rows[0].max_connections),
+        current_connections: dbCurrentCons.rows[0].count
+      }
+    }
+  });
 }
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,35 @@
-test("GET to /api/v1/status shuld return 200", async () => {
-  const res = await fetch("http://localhost:3000/api/v1/status");
+const baseUrl = "http://localhost:3000" 
+test("GET to /api/v1/status should return 200", async () => {
+  const res = await fetch(baseUrl+"/api/v1/status");
   expect(res.status).toBe(200);
+});
+
+test("GET to /api/v1/status to have all properties", async () => {
+
+  const res = await fetch(baseUrl+"/api/v1/status");
+  const resBody = await res.json();
+
+  expect(resBody.updated_at).toBeDefined();
+
+  expect(resBody.dependencies.database.version).toBeDefined();
+
+  expect(resBody.dependencies.database.max_connections).toBeDefined();
+
+  expect(resBody.dependencies.database.current_connections).toBeDefined();
+});
+
+test("GET to /api/v1/status to return valid properties", async () => {
+  const res = await fetch(baseUrl+"/api/v1/status");
+  const resBody = await res.json();
+  const parsedUpdatedAt = new Date(resBody.updated_at).toISOString();
+
+  expect(resBody.updated_at).toEqual(parsedUpdatedAt);
+
+  expect(typeof resBody.dependencies.database.version).toBe('string');
+
+  expect(typeof resBody.dependencies.database.max_connections).toBe('number');
+  expect(resBody.dependencies.database.max_connections > -1).toBe(true);
+
+  expect(typeof resBody.dependencies.database.current_connections).toBe('number');
+  expect(resBody.dependencies.database.current_connections === 1).toBe(true); 
 });
