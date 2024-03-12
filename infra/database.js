d6541b3e0ac87ba35970c83ae0fb114ea419e479
Cabeludo <brepimentel2001@gmail.com>
2024-03-12 00:26:54 -0300

    melhora ssl para db

--- infra/database.js
+++ infra/database.js
@@ -1,13 +1,18 @@
 import { Client } from "pg";
+
+const sslActive = process.env.NODE_ENV === 'production' ? true : false
+
 async function query(queryObj) {
+
   const client = new Client({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: sslActive
   });
-  
+
   let result = null;
   try {
     await client.connect();
