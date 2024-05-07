11aeb193f10a994f2ede720b63b6787b3dbabe92
Cabeludo <brepimentel2001@gmail.com>
2024-05-07 02:00:48 -0300

    ajuste na chamada para não executar nada caso o metodo não tenha tratamento

--- tests/integration/api/v1/migrations/put.test.js
+++ tests/integration/api/v1/migrations/put.test.js
@@ -8,16 +8,22 @@ async function cleanDatabase() {
   await database.query('drop schema public cascade; create schema public;');
 }
 
-test("PUT to /api/v1/migrations to return only one connection after the unhandled method", async () => {
-  await fetch(baseUrl+"/api/v1/migrations", {
-    method: 'PUT'
+test("/api/v1/migrations to return only one connection after the unhandled method", async () => {
+  const callMethod = "PUT";
+  const testCall = await fetch(baseUrl+"/api/v1/migrations", {
+    method: callMethod
   });
   await fetch(baseUrl+"/api/v1/migrations", {
-    method: 'PUT'
+    method: callMethod
   });
   
+  const testCallBody = await testCall.json();
+  expect(testCallBody.error).toBeDefined();
+  expect(testCallBody.error).toBe(`Method "${callMethod}" not allowed`); 
+
   const res = await fetch(baseUrl+"/api/v1/status");
   const resBody = await res.json();
 
+  
   expect(resBody.dependencies.database.current_connections).toBe(1); 
 });
