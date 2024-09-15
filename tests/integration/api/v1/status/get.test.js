48c3e5db65737aa315ad6718e81309c8363f9f10
Breno Pimentel da Silva <brenocontato2001@gmail.com>
2024-09-15 00:53:16 -0300

    fix: put tests under `Anonymous user` `describe`

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -12,36 +12,40 @@ describe("GET /api/v1/status", () => {
       const res = await fetch(baseUrl + "/api/v1/status");
       expect(res.status).toBe(200);
     });
-  });
 
-  test("Have all properties", async () => {
-    const res = await fetch(baseUrl + "/api/v1/status");
-    const resBody = await res.json();
+    test("Have all properties", async () => {
+      const res = await fetch(baseUrl + "/api/v1/status");
+      const resBody = await res.json();
 
-    expect(resBody.updated_at).toBeDefined();
+      expect(resBody.updated_at).toBeDefined();
 
-    expect(resBody.dependencies.database.version).toBeDefined();
+      expect(resBody.dependencies.database.version).toBeDefined();
 
-    expect(resBody.dependencies.database.max_connections).toBeDefined();
+      expect(resBody.dependencies.database.max_connections).toBeDefined();
 
-    expect(resBody.dependencies.database.current_connections).toBeDefined();
-  });
+      expect(resBody.dependencies.database.current_connections).toBeDefined();
+    });
 
-  test("Return valid properties", async () => {
-    const res = await fetch(baseUrl + "/api/v1/status");
-    const resBody = await res.json();
-    const parsedUpdatedAt = new Date(resBody.updated_at).toISOString();
+    test("Return valid properties", async () => {
+      const res = await fetch(baseUrl + "/api/v1/status");
+      const resBody = await res.json();
+      const parsedUpdatedAt = new Date(resBody.updated_at).toISOString();
 
-    expect(resBody.updated_at).toEqual(parsedUpdatedAt);
+      expect(resBody.updated_at).toEqual(parsedUpdatedAt);
 
-    expect(typeof resBody.dependencies.database.version).toBe("string");
+      expect(typeof resBody.dependencies.database.version).toBe("string");
 
-    expect(typeof resBody.dependencies.database.max_connections).toBe("number");
-    expect(resBody.dependencies.database.max_connections > -1).toBe(true);
+      expect(typeof resBody.dependencies.database.max_connections).toBe(
+        "number",
+      );
+      expect(resBody.dependencies.database.max_connections > -1).toBe(true);
 
-    expect(typeof resBody.dependencies.database.current_connections).toBe(
-      "number",
-    );
-    expect(resBody.dependencies.database.current_connections === 1).toBe(true);
+      expect(typeof resBody.dependencies.database.current_connections).toBe(
+        "number",
+      );
+      expect(resBody.dependencies.database.current_connections === 1).toBe(
+        true,
+      );
+    });
   });
 });
