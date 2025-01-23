8119276fd682a828f63e4a90f76e56d7614990e4
Breno Pimentel da Silva <brenocontato2001@gmail.com>
2025-01-23 14:10:36 -0300

    feat: create and use `InternalServerError` in `/api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,25 +1,35 @@
 import database from "infra/database.js";
+import { InternalServerError } from "infra/errors";
 
 export default async function status(req, res) {
-  const dbVersion = await database.query("SHOW server_version;");
+  try {
+    const dbVersion = await database.query("SHOW server_version;");
 
-  const dbMaxCons = await database.query("SHOW MAX_CONNECTIONS;");
+    const dbMaxCons = await database.query("SHOW MAX_CONNECTIONS;");
 
-  const databaseName = process.env.POSTGRES_DB;
-  const dbCurrentCons = await database.query({
-    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
-    values: [databaseName],
-  });
+    const databaseName = process.env.POSTGRES_DB;
+    const dbCurrentCons = await database.query({
+      text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+      values: [databaseName],
+    });
 
-  const updatedAt = new Date().toISOString();
-  res.status(200).json({
-    updated_at: updatedAt,
-    dependencies: {
-      database: {
-        version: dbVersion.rows[0].server_version,
-        max_connections: parseInt(dbMaxCons.rows[0].max_connections),
-        current_connections: dbCurrentCons.rows[0].count,
+    const updatedAt = new Date().toISOString();
+    res.status(200).json({
+      updated_at: updatedAt,
+      dependencies: {
+        database: {
+          version: dbVersion.rows[0].server_version,
+          max_connections: parseInt(dbMaxCons.rows[0].max_connections),
+          current_connections: dbCurrentCons.rows[0].count,
+        },
       },
-    },
-  });
+    });
+  } catch (error) {
+    const publicErrorObj = new InternalServerError({
+      cause: error,
+    });
+    console.log("\n Erro dentro do catch do controller:");
+    console.error(publicErrorObj);
+    res.status(500).json(publicErrorObj);
+  }
 }
